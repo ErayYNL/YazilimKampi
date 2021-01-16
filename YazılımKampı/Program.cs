5551b459a352e86381abdd41989b194dec048d70
Eray Yanlı <yanlilexo02@gmail.com>
2021-01-16 23:56:20 +0300

    16.01.2021

---
+++ YazılımKampı/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        //type safety = tip güvenliği -- Metinsel içerikler çift tırnak ile yapılır. ";" zorunlu. //Console.WriteLine ("Hello World");
+        //Do not repeat yourself. = Kendini tekrarlama.
+        //alias = değer 
+
+        string kategoriEtiketi = "Kategoriler";
+        int ogrenciSayisi = 32000;
+        double faizOrani = 1.45;  //ondalıklı sayı
+        bool sistemeGirisYapmisMi = true;
+        double dolarDun = 7.35;
+        double dolarBugun = 7.45;
+
+        if (dolarDun > dolarBugun)
+        {
+            Console.WriteLine("Azalış butonu");
+        }
+        else if (dolarDun < dolarBugun)
+        {
+            Console.WriteLine("Artış butonu");
+        }
+        else
+        {
+            Console.WriteLine("Değişmedi butonu");
+        }
+
+        if (sistemeGirisYapmisMi == true)
+        {
+            Console.WriteLine("Kullanıcı ayarları butonu");
+        }
+        else
+        {
+            Console.WriteLine("Giriş yap butonu");
+        }
+
+        Console.WriteLine(kategoriEtiketi);
+    }
+}
