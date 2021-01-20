73b7e4a5ddc31ce73b02c3b4c561459958017874
Eray Yanlı <yanlilexo02@gmail.com>
2021-01-20 23:55:02 +0300

    YazilimKampi

---
+++ Koleksiyonlar/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Koleksiyonlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] isimler = new string[] { "Eray", "Can", "Berke", "Eren" };   //ctrl+k+c ctrl+k+u
+            //Console.WriteLine(isimler[0]);
+            //Console.WriteLine(isimler[1]);
+            //Console.WriteLine(isimler[2]);
+            //Console.WriteLine(isimler[3]);
+
+            //isimler = new string[5];  // new = yeni referans. new'den sonra yukarideki değerler kaybedilir yani 0. eleman boş gelir.
+            //isimler[4] = "Kaan";
+            //Console.WriteLine(isimler[4]);
+            //Console.WriteLine(isimler[0]);
+
+            List<string> isimler2 = new List<string> { "Eray", "Can", "Berke", "Eren" };
+            Console.WriteLine(isimler2[0]);
+            Console.WriteLine(isimler2[1]);
+            Console.WriteLine(isimler2[2]);
+            Console.WriteLine(isimler2[3]);
+            isimler2.Add("Kaan");
+            Console.WriteLine(isimler2[4]);
+            Console.WriteLine(isimler2[0]);
+
+        }
+    }
+}
